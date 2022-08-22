3605d0ff679cfed4aab80850152413dd7971f8d1
IskenderDuisheev <isken2014@gmail.com>
2022-08-22 16:45:09 +0600

    change

--- config/settings.py
+++ config/settings.py
@@ -93,6 +93,8 @@ DATABASES = {
 }
 
 
+
+
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -111,6 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
