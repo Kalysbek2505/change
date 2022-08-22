96e3471ec8f301a62f97f12064bda9e9797f671f
ertayx <ertaiesen05@gmail.com>
2022-08-22 16:55:30 +0600

    .

--- accounts/models.py
+++ accounts/models.py
@@ -22,12 +22,28 @@ class UserManager(BaseUserManager):
             raise ValueError('Требуется электронная почта')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_field)
-        user = set_password = (password)
+        user.set_password(password)
         user.is_active = True
         user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
         return user
+
+        # def create_superuser(self, email, password, **extra_fields):
+        # if not email:
+        #     raise ValueError("Email is required")
+        
+        # email = self.normalize_email(email)
+        # user = self.model(email=email, **extra_fields)
+        # user.set_password(password)
+
+        # user.is_active = True
+        # user.is_superuser = True
+        # user.is_staff = True
+
+        # user.save(using=self._db)
+        # return user
+
 
 
 class User(AbstractUser):
